79320ff60945bbfafefb08f7f2b63292794a81e4
Tagir Sultanov <togisrisa@gmail.com>
2026-06-19 05:33:54 +0300

    Add ReverseLinkedList problem solution
    Signed-off-by: Tagir Sultanov <togisrisa@gmail.com>

---
+++ cpp/ReverseLinkedList.cpp
@@ -0,0 +1,30 @@
+#include <cassert>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode *head) {
+        ListNode *next = nullptr;
+        ListNode *curr = head;
+        ListNode *prev = nullptr;
+
+        while (curr != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
+
+int main() {
+}
